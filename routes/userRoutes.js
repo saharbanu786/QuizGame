7b404c3be958772c7615f1619b6be19a88a3a17d
Saharbanu <saharbanu.a2023cse@sece.ac.in>
2025-01-28 12:12:01 +0530

    commited

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -102,4 +102,4 @@ router.post("/logout", (req, res) => {
   res.status(200).json({ message: "Logout successful" });
 });
 
-module.exports = router;
+module.exports = router; 
--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.use("/api/users", userRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running")); 
